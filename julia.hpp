20d95bbfec0552b66b27a1caced7bfe1db93afdb
giuliomagnifico <giuliomagnifico@gmail.com>
2024-09-29 01:27:40 +0200

    Update julia.hpp

--- julia.hpp
+++ julia.hpp
@@ -1,28 +1,27 @@
-#ifndef JULIA_HPP
-#define JULIA_HPP
-
-#include "GUI_Paint.h"
 #include "DEV_Config.h"
-#include <vector>
-#include <tuple>
 
-class JuliaSet {
+class JuliaSet
+{
 public:
-    JuliaSet(double cx = -0.7, double cy = 0.27015);
-    
-    void SetRender(UBYTE* image);
+    void InitJuliaSet();
     void Render(UWORD xResolution, UWORD yResolution);
-    bool IsJuliaPoint(double fX, double fY, int iterations);
-    unsigned long long GetUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv);
-    bool IsAreaUniform(int xOffset, int yOffset, double fW, double fH, int wDiv, int hDiv, double wStart, double hStart);
-    double GetImprovedUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv);
+    void SetRender(UBYTE* image);
+    UBYTE* GetRender() { return rendered; };
     void ZoomOnInterestingArea();
 
 private:
-    double c_x, c_y; 
-    double w, h, x, y;
-    UBYTE* rendered;
-    UWORD renderedResX, renderedResY;
-};
+    bool IsJuliaPoint(double x, double y, int iterations);
+    unsigned long long GetUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv);
+    bool IsAreaUniform(int xOffset, int yOffset, double fW, double fH,  int wDiv, int hDiv, double wStart, double hStart);
 
-#endif
+
+    double GetImprovedUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv);
+
+    UBYTE* rendered;
+    double w;
+    double h;
+    double x;
+    double y;
+    UWORD renderedResX;
+    UWORD renderedResY;
+};
