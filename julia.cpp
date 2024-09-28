f5c5f8ef1dc152dbdf1b040e58795f4a0f2b3adb
giuliomagnifico <giuliomagnifico@gmail.com>
2024-09-29 01:27:07 +0200

    Update julia.cpp
    Improved algorithm

--- julia.cpp
+++ julia.cpp
@@ -1,92 +1,183 @@
 #include "julia.hpp"
-#include <algorithm>
-#include <cmath>
+#include <complex>
 #include <random>
-#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include "GUI_Paint.h"
 
-using namespace std;
+void JuliaSet::InitJuliaSet()
+{
+    w = 4;
+    h = 2;
+    x = 0;
+    y = 0;
+    rendered = NULL;
+    renderedResX = 0;
+    renderedResY = 0;
+    srand(time(0));  
+}
 
-JuliaSet::JuliaSet(double cx, double cy)
-    : c_x(cx), c_y(cy), w(4), h(2), x(0), y(0), rendered(nullptr), renderedResX(0), renderedResY(0) {}
-
-void JuliaSet::SetRender(UBYTE* image) {
+void JuliaSet::SetRender(UBYTE* image)
+{
     rendered = image;
 }
 
-void JuliaSet::Render(UWORD xResolution, UWORD yResolution) {
-    int iter = (100 + max(0.0, -log10(w)) * 200); 
-    vector<vector<bool>> columns;
+void JuliaSet::Render(UWORD xResolution, UWORD yResolution)
+{
+    static int imageIndex = 0;
+    bool validImage = false;
 
-    for (int i = yResolution - 1; i >= 0; --i) {
-        vector<bool> rows;
-        for (int j = 0; j < xResolution; ++j) {
-            double p_x = this->x - this->w / 2.0 + (double)j / (double)xResolution * this->w;
-            double p_y = this->y - this->h / 2.0 + (double)(i + 1) / (double)yResolution * this->h;
-            rows.emplace_back(IsJuliaPoint(p_x, p_y, iter)); 
+    while (!validImage) {
+        double aspectRatio = (double)xResolution / (double)yResolution;
+
+        if (imageIndex == 0) {
+            if (w / h != aspectRatio) {
+                h = w / aspectRatio;
+            }
+        } else if (imageIndex < 5) {
+            x += ((rand() % 100) - 50) / 500.0;
+            y += ((rand() % 100) - 50) / 500.0;
+            w *= 1.0 + ((rand() % 40) - 20) / 100.0;
+            h *= 1.0 + ((rand() % 40) - 20) / 100.0;
+
+            w = std::min(std::max(w, 0.05), 4.0);
+            h = std::min(std::max(h, 0.05), 2.0);
+            h = w / aspectRatio;
+            x = std::min(std::max(x, -1.5), -0.5);
+            y = std::min(std::max(y, -0.5), 0.5);
+        } else {
+            x += ((rand() % 50) - 25) / 1000.0;
+            y += ((rand() % 50) - 25) / 1000.0;
+            w *= 1.0 + ((rand() % 20) - 10) / 100.0;
+            h *= 1.0 + ((rand() % 20) - 10) / 100.0;
+
+            w = std::min(std::max(w, 0.05), 4.0);
+            h = std::min(std::max(h, 0.05), 2.0);
+            h = w / aspectRatio;
+            x = std::min(std::max(x, -1.5), -0.5);
+            y = std::min(std::max(y, -0.5), 0.5);
         }
-        columns.emplace_back(rows);
-    }
 
-    renderedResX = xResolution;
-    renderedResY = yResolution;
+        imageIndex++;
 
-    for (unsigned int y = 0; y < columns.size(); ++y) {
-        auto row = columns[y];
-        for (unsigned int x = 0; x < row.size(); ++x) {
-            auto bitSet = row[x];
-            if (bitSet) {
-                Paint_SetPixel(x, y, WHITE);
-            } else {
-                Paint_SetPixel(x, y, BLACK);
+        int iter = (50 + std::max(0.0, -log10(w)) * 100);
+        iter += rand() % 50;
+
+        std::vector<std::vector<bool>> columns;
+        int whiteCount = 0;
+        int blackCount = 0;
+
+        for (int i = yResolution - 1; i >= 0; --i) {
+            std::vector<bool> rows;
+            for (int j = 0; j < xResolution; ++j) {
+                double p_x = this->x - this->w / 2.0 + (double)j / (double)xResolution * this->w;
+                double p_y = this->y - this->h / 2.0 + (double)(i + 1) / (double)yResolution * this->h;
+                bool isJuliaPoint = IsJuliaPoint(p_x, p_y, iter);
+                rows.emplace_back(isJuliaPoint);
+
+                if (isJuliaPoint) {
+                    blackCount++;
+                } else {
+                    whiteCount++;
+                }
+            }
+            columns.emplace_back(rows);
+        }
+
+        renderedResX = xResolution;
+        renderedResY = yResolution;
+
+        double totalPixels = (double)(xResolution * yResolution);
+        double blackRatio = (double)blackCount / totalPixels;
+        double whiteRatio = (double)whiteCount / totalPixels;
+
+        if (blackRatio < 0.95 && whiteRatio < 0.95) {
+            validImage = true;
+
+            for (unsigned int y = 0; y < columns.size(); ++y) {
+                auto row = columns[y];
+                for (unsigned int x = 0; x < row.size(); ++x) {
+                    auto bitSet = row[x];
+                    if (bitSet) {
+                        Paint_SetPixel(x, y, BLACK);
+                    } else {
+                        Paint_SetPixel(x, y, WHITE);
+                    }
+                }
             }
         }
     }
 }
 
-bool JuliaSet::IsJuliaPoint(double z_x, double z_y, int iterations) {
+bool JuliaSet::IsJuliaPoint(double fX, double fY, int iterations)
+{
+    std::complex<double> z(fX, fY);
+    std::complex<double> c(-0.7, 0.27015);  
+
     for (int i = 0; i < iterations; ++i) {
-        double z_x_old = z_x;
-        z_x = z_x * z_x - z_y * z_y + c_x;
-        z_y = 2.0 * z_x_old * z_y + c_y;
-        if (z_x * z_x + z_y * z_y > 4) {
+        z = z * z + c;
+        if (std::abs(z) > 2.0) {
             return true;
         }
     }
     return false;
 }
 
-unsigned long long JuliaSet::GetUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv) {
-    unsigned long long uniformness = 0;
-    for (int wStart = 0; wStart < wDiv; ++wStart) {
-        for (int hStart = 0; hStart < hDiv; ++hStart) {
-            if (IsAreaUniform(xOffset, yOffset, fW, fH, wDiv, hDiv, wStart, hStart)) {
-                ++uniformness;
-            }
-        }
-    }
+void JuliaSet::ZoomOnInterestingArea()
+{
+    std::vector<std::tuple<double, double, double>> choices;
 
-    return uniformness;
+    auto uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, 0, 0, 2, 2);
+    choices.emplace_back(this->x - this->w / 4, this->y + this->h / 4, uniformness);
+
+    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, this->renderedResX / 2, 0, 2, 2);
+    choices.emplace_back(this->x + this->w / 4, this->y + this->h / 4, uniformness);
+
+    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, 0, this->renderedResY / 2, 2, 2);
+    choices.emplace_back(this->x - this->w / 4, this->y - this->h / 4, uniformness);
+
+    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, this->renderedResX / 2, this->renderedResY / 2, 2, 2);
+    choices.emplace_back(this->x + this->w / 4, this->y - this->h / 4, uniformness);
+
+    w = w / 2.0;
+    h = h / 2.0;
+
+    auto lessUniformChoices = choices;
+    lessUniformChoices.erase(std::remove_if(lessUniformChoices.begin(), lessUniformChoices.end(),
+                                            [](const std::tuple<double, double, double>& x) { return std::get<2>(x) >= 0.95; }),
+                             lessUniformChoices.end());
+
+    auto topTierChoices = choices;
+    topTierChoices.erase(std::remove_if(topTierChoices.begin(), topTierChoices.end(),
+                                        [](const std::tuple<double, double, double>& x) { return std::get<2>(x) >= 0.85; }),
+                         topTierChoices.end());
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    if (!topTierChoices.empty()) {
+        std::shuffle(topTierChoices.begin(), topTierChoices.end(), g);
+        auto selection = topTierChoices[0];
+        this->x = std::get<0>(selection);
+        this->y = std::get<1>(selection);
+    } else if (!lessUniformChoices.empty()) {
+        std::shuffle(lessUniformChoices.begin(), lessUniformChoices.end(), g);
+        auto selection = lessUniformChoices[0];
+        this->x = std::get<0>(selection);
+        this->y = std::get<1>(selection);
+    } else {
+        std::shuffle(choices.begin(), choices.end(), g);
+        auto selection = choices[0];
+        this->x = std::get<0>(selection);
+        this->y = std::get<1>(selection);
+    }
 }
 
-bool JuliaSet::IsAreaUniform(int xOffset, int yOffset, double fW, double fH, int wDiv, int hDiv, double wStart, double hStart) {
-    int yInit = yOffset + static_cast<int>(fH / hDiv) * hStart;
-    int xInit = xOffset + static_cast<int>(fW / wDiv) * wStart;
-    auto firstPoint = Paint_GetPixel(xInit, yInit);
-
-    for (unsigned int i = 0; i < static_cast<unsigned int>(fW / wDiv); ++i) {
-        for (unsigned int j = 0; j < static_cast<unsigned int>(fH / hDiv); ++j) {
-            int yTest = yOffset + static_cast<int>(fH / hDiv) * hStart + j;
-            int xTest = xOffset + static_cast<int>(fW / wDiv) * wStart + i;
-            auto testPoint = Paint_GetPixel(xTest, yTest);
-            if (testPoint != firstPoint)
-                return false;
-        }
-    }
-
-    return true;
-}
-
-double JuliaSet::GetImprovedUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv) {
+double JuliaSet::GetImprovedUniformnessOfArea(double fW, double fH, int xOffset, int yOffset, int wDiv, int hDiv)
+{
     unsigned long long numWhite = 0;
     unsigned long long numBlack = 0;
     double totalPixels = fW * fH;
@@ -102,66 +193,5 @@ double JuliaSet::GetImprovedUniformnessOfArea(double fW, double fH, int xOffset,
         }
     }
 
-    return max((double)numWhite / totalPixels, (double)numBlack / totalPixels);
-}
-
-void JuliaSet::ZoomOnInterestingArea() {
-
-    tuple<double, double, double> choice;
-    vector<tuple<double, double, double>> choices;
-
-    auto uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, 0, 0, 2, 2);
-    choice = {this->x - this->w / 4, this->y + this->h / 4, uniformness};
-    choices.emplace_back(choice);
-
-    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, this->renderedResX / 2, 0, 2, 2);
-    choice = {this->x + this->w / 4, this->y + this->h / 4, uniformness};
-    choices.emplace_back(choice);
-
-    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, 0, this->renderedResY / 2, 2, 2);
-    choice = {this->x - this->w / 4, this->y - this->h / 4, uniformness};
-    choices.emplace_back(choice);
-
-    uniformness = GetImprovedUniformnessOfArea(this->renderedResX / 2, this->renderedResY / 2, this->renderedResX / 2, this->renderedResY / 2, 2, 2);
-    choice = {this->x + this->w / 4, this->y - this->h / 4, uniformness};
-    choices.emplace_back(choice);
-
-    w = w / 2.0;
-    h = h / 2.0;
-
-    auto lessUniformChoices = choices;
-    lessUniformChoices.erase(std::remove_if(
-        lessUniformChoices.begin(),
-        lessUniformChoices.end(),
-        [](const tuple<double, double, double>& x) {
-            return (std::get<2>(x) >= 0.95);
-        }), lessUniformChoices.end());
-
-    auto topTierChoices = choices;
-    topTierChoices.erase(std::remove_if(
-        topTierChoices.begin(),
-        topTierChoices.end(),
-        [](const tuple<double, double, double>& x) {
-            return (std::get<2>(x) >= 0.85);
-        }), topTierChoices.end());
-
-    random_device rd;
-    mt19937 g(rd());
-
-    if (topTierChoices.size() > 0) {
-        shuffle(topTierChoices.begin(), topTierChoices.end(), g);
-        auto selection = topTierChoices[0];
-        this->x = get<0>(selection);
-        this->y = get<1>(selection);
-    } else if (lessUniformChoices.size() > 0) {
-        shuffle(lessUniformChoices.begin(), lessUniformChoices.end(), g);
-        auto selection = lessUniformChoices[0];
-        this->x = get<0>(selection);
-        this->y = get<1>(selection);
-    } else {
-        shuffle(choices.begin(), choices.end(), g);
-        auto selection = choices[0];
-        this->x = get<0>(selection);
-        this->y = get<1>(selection);
-    }
+    return std::max((double)numWhite / totalPixels, (double)numBlack / totalPixels);
 }
