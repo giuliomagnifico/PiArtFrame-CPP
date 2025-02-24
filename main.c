439150e55e8079e209f35622d4a020108a0d81d3
giuliomagnifico <giuliomagnifico@gmail.com>
2025-02-25 00:34:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,81 +5,88 @@
 #include <time.h>
 #include <iostream>
 #include <chrono>
+#include <unistd.h>  // For sleep()
 #include "julia.hpp"
 
 using namespace std;
 using namespace chrono;
 
-static constexpr unsigned long SecondsBetweenImages = 30 * 60;
+// Define global variables
+static constexpr unsigned long SecondsBetweenImages = 60 * 60;
+
+// SIGINT Handler function
 void Handler(int signo) {
-
-  printf("\r\nHandler:exit\r\n");
-  DEV_Module_Exit();
-
-  exit(0);
+    printf("\r\nHandler: exit\r\n");
+    DEV_Module_Exit();
+    exit(0);
 }
 
 int main(void) {
+    // Register signal handler
+    signal(SIGINT, Handler);
 
-  signal(SIGINT, Handler);
-
-  if (DEV_Module_Init() != 0) {
-    return -1;
-  }
-
-  printf("e-Paper Init...\r\n");
-  EPD_7IN5_V2_Init();
-  EPD_7IN5_V2_Clear();
-  DEV_Delay_ms(500);
-
-  UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
-  UBYTE * img = NULL;
-
-  if ((img = (UBYTE * ) malloc(Imagesize)) == NULL) {
-    printf("Failed to apply for image memory...\r\n");
-    return -1;
-  }
-  Paint_NewImage(img, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
-  Paint_SelectImage(img);
-
-  JuliaSet julia;
-  julia.InitJuliaSet();
-  julia.SetRender(img);
-
-  bool isFirstImage = true;
-  unsigned int numberOfZooms = 1;
-  while (true) {
-    steady_clock::time_point beforeRender = steady_clock::now();
-    cout << "Starting render..." << endl;
-    julia.Render(EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT);
-    cout << "Render complete!" << endl;
-    steady_clock::time_point afterRender = steady_clock::now();
-
-    if (isFirstImage) {
-      isFirstImage = false;
-    } else {
-      while (duration_cast < std::chrono::seconds > (afterRender - beforeRender).count() < SecondsBetweenImages) {
-        sleep(5);
-        afterRender = steady_clock::now();
-      }
+    if (DEV_Module_Init() != 0) {
+        return -1;
     }
-    cout << "Drawing image..." << endl;
 
+    printf("e-Paper Init...\r\n");
     EPD_7IN5_V2_Init();
     EPD_7IN5_V2_Clear();
     DEV_Delay_ms(500);
-    EPD_7IN5_V2_Display(img);
-    EPD_7IN5_V2_Sleep();
-    cout << "Draw completed!" << endl;
 
-    julia.ZoomOnInterestingArea();
+    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? 
+                        (EPD_7IN5_V2_WIDTH / 8) : 
+                        (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+    UBYTE *img = NULL;
 
-    if (numberOfZooms % 50 == 0) {
-      julia.InitJuliaSet();
+    if ((img = (UBYTE *) malloc(Imagesize)) == NULL) {
+        printf("Failed to allocate image memory...\r\n");
+        return -1;
     }
-    numberOfZooms++;
 
-  }
+    Paint_NewImage(img, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
+    Paint_SelectImage(img);
 
-  return 0;
+    JuliaSet julia;
+    julia.InitJuliaSet();
+    julia.SetRender(img);
+
+    bool isFirstImage = true;
+    unsigned int numberOfZooms = 1;
+
+    while (true) {
+        steady_clock::time_point beforeRender = steady_clock::now();
+        cout << "Starting render..." << endl;
+        julia.Render(EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT);
+        cout << "Render complete!" << endl;
+        steady_clock::time_point afterRender = steady_clock::now();
+
+        if (!isFirstImage) {
+            while (static_cast<unsigned long>(duration_cast<seconds>(afterRender - beforeRender).count()) < SecondsBetweenImages) {
+                sleep(5);
+                afterRender = steady_clock::now();
+            }
+        } else {
+            isFirstImage = false;
+        }
+
+        cout << "Drawing image..." << endl;
+
+        EPD_7IN5_V2_Init();
+        EPD_7IN5_V2_Clear();
+        DEV_Delay_ms(500);
+        EPD_7IN5_V2_Display(img);
+        EPD_7IN5_V2_Sleep();
+        cout << "Draw completed!" << endl;
+
+        julia.ZoomOnInterestingArea();
+
+        if (numberOfZooms % 50 == 0) {
+            julia.InitJuliaSet();
+        }
+        numberOfZooms++;
+    }
+
+    free(img);
+    return 0;
 }
